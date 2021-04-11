8ac0925a203a1e5aa81f4f1ff398bf8f4fef1aed
Jeff Eldredge <jdeldre@g.ucla.edu>
2021-04-10 19:04:16 -0700

    Temporarily removed gallons to get past annoying error for some users

--- src/units.jl
+++ src/units.jl
@@ -1,4 +1,4 @@
 # Put additional units here
 @unit lbm        "lbm"       PoundMass   1Unitful.lb            false
 @unit mmHg       "mmHg"      MMHg        (1//760)Unitful.atm      false
-@unit gal        "gal"       USGallon    231Unitful.inch^3    false
+#@unit gal        "gal"       USGallon    231Unitful.inch^3    false
